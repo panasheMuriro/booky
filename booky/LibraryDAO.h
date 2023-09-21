818bbfe081c48a976491457314458670a4f0416f
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-21 14:56:17 -0500

    Update DAO for books and authors

--- booky/LibraryDAO.h
+++ booky/LibraryDAO.h
@@ -6,11 +6,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Book.h"
+#import "Author.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LibraryDAO : NSObject
-
++(instancetype) sharedInstance;
+-(NSArray<Book *> *)getAllBooks;
+-(NSArray<Author *> *)getAllAuthors;
+-(void)addBookWithTitle:(NSString *)title author:(Author *)author;
 @end
 
 NS_ASSUME_NONNULL_END
--- booky/LibraryDAO.m
+++ booky/LibraryDAO.m
@@ -7,6 +7,49 @@
 
 #import "LibraryDAO.h"
 
+@interface LibraryDAO()
+@property (nonatomic, strong) NSMutableArray<Book *> *books;
+@property (nonatomic, strong) NSMutableArray<Author *> *authors;
+@end
+
 @implementation LibraryDAO
++ (instancetype)sharedInstance{
+    static LibraryDAO *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init];
+    });
+    return sharedInstance;
+}
+
+-(instancetype) init{
+    self = [super init];
+    if(self){
+        _books = [NSMutableArray array];
+        _authors = [NSMutableArray array];
+    }
+    return self;
+}
+
+-(NSArray<Book *>*)getAllBooks{
+    return [self.books copy];
+}
+
+- (NSArray<Author *> *)getAllAuthors{
+    return [self.authors copy];
+}
+
+- (void)addBookWithTitle:(NSString *)title author:(Author *)author{
+    Book *book = [[Book alloc] initWithTitle:title author:author];
+    [self.books addObject:book];
+}
+
+
+
+
+
+
+
 
 @end
