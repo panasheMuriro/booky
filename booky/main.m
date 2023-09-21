f6696754d3e336eefd6b3c3ac9a2398c8527b035
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-21 10:21:35 -0500

    Initial Commit

---
+++ booky/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  booky
+//
+//  Created by Panashe on 9/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
