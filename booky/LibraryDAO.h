35d047608517e24912906b81eaab24d2500e818d
murirop <panashemuriro2023@u.northwestern.edu>
2023-09-21 14:55:54 -0500

    Add DAO for books and author

---
+++ booky/LibraryDAO.h
@@ -0,0 +1,16 @@
+//
+//  LibraryDAO.h
+//  booky
+//
+//  Created by Panashe on 9/21/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LibraryDAO : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ booky/LibraryDAO.m
@@ -0,0 +1,12 @@
+//
+//  LibraryDAO.m
+//  booky
+//
+//  Created by Panashe on 9/21/23.
+//
+
+#import "LibraryDAO.h"
+
+@implementation LibraryDAO
+
+@end
